de2a9464828e2d9bcaa8b54ab73e802717b6c62e
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-12-21 11:37:36 +0700

    update to current location

--- js/wisataDekat.js
+++ js/wisataDekat.js
@@ -1,7 +1,11 @@
+let currentLat = 0;
+let currentLng = 0;
+
+
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -6.185689700868288, lng: 106.7789113971096 };
+  const uluru = { lat: currentLat, lng: currentLng };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
@@ -15,34 +19,48 @@ function initMap() {
 }
 
 function getNearbyAttractions() {
-    const request = {
-    location: new google.maps.LatLng(-6.185689700868288, 106.7789113971096),
+  const request = {
+    location: new google.maps.LatLng(currentLat, currentLng),
     radius: 5000,
     type: ['point of interest']
-    };
-    let ol = document.getElementById("list-places");
-   
+  };
+  let ol = document.getElementById("list-places");
+  
+  
+  const places = document.getElementById('places');
+  const service = new google.maps.places.PlacesService(places);
+  
+  service.nearbySearch(request, (data) => {
+    for(let i = 0; i < data.length; i++){
+      if(i == 0 || i == 19){
+        continue;
+      } else {
+        let li = document.createElement("li");
+        li.setAttribute('class','item');
+        ol.appendChild(li);
+        let text = document.createTextNode(data[i]);
+        li.innerHTML = li.innerHTML + data[i].name;
+      }
+    }
+  });
+}
 
-    const places = document.getElementById('places');
-    const service = new google.maps.places.PlacesService(places);
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    alert("Geolocation is not supported by this browser.");
+  }
+}
 
-    service.nearbySearch(request, (data) => {
-        for(let i = 0; i < data.length; i++){
-            if(i == 0 || i == 19){
-                continue;
-            } else {
-                let li = document.createElement("li");
-                li.setAttribute('class','item');
-                ol.appendChild(li);
-                let text = document.createTextNode(data[i]);
-                li.innerHTML = li.innerHTML + data[i].name;
-            }
-        }
-    });
+function showPosition(position) {
+  currentLat = position.coords.latitude;
+  currentLng = position.coords.longitude;
+  initMap();
 }
 
 const init = function () {
-  initMap();
+  getLocation();
   getNearbyAttractions();
 };
 
